15c46d91275ac7d41fc0f596b982be93021b0076
Caden Jarrett <98786948+caden-jarrett@users.noreply.github.com>
2023-01-16 14:13:35 -0700

    working things out

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Game, type: :model do
     describe "validations" do
         it {should validate_presence_of(:sport)}
         it {should validate_presence_of(:date)}
-        # it {should validate_presence_of(:teams)}
+        it {should validate_presence_of(:teams)}
     end
 end
